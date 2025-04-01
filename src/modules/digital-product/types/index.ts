52df0aeb8c67632ff21c490e556a16af5b538dcf
Osiris8 <miganosiris8@gmaaail.com>
2025-04-01 14:42:37 +0200

    add module definition

--- src/modules/digital-product/types/index.ts
+++ src/modules/digital-product/types/index.ts
@@ -1,3 +1,12 @@
+import DigitalProductModuleService from "../service";
+import { Module } from "@medusajs/framework/utils";
+
+export const DIGITAL_PRODUCT_MODULE = "digitalProductModuleService";
+
+export default Module(DIGITAL_PRODUCT_MODULE, {
+  service: DigitalProductModuleService,
+});
+
 export enum MediaType {
   MAIN = "main",
   PREVIEW = "preview",
