3fcfe8fef437b6e38445c4dfdaf39752d459a5b7
Osiris8 <miganosiris8@gmaaail.com>
2025-04-01 23:16:00 +0200

    add API Route POST for digital products

--- src/api/admin/digital-products/route.ts
+++ src/api/admin/digital-products/route.ts
@@ -4,6 +4,11 @@ import {
 } from "@medusajs/framework/http";
 import { ContainerRegistrationKeys } from "@medusajs/framework/utils";
 
+import { z } from "zod";
+import createDigitalProductWorkflow from "../../../workflows/create-digital-product";
+import { CreateDigitalProductMediaInput } from "../../../workflows/create-digital-product/steps/create-digital-product-medias";
+import { createDigitalProductsSchema } from "../../validation-schemas";
+
 export const GET = async (
   req: AuthenticatedMedusaRequest,
   res: MedusaResponse
@@ -26,5 +31,41 @@ export const GET = async (
     count,
     limit: take,
     offset: skip,
+  });
+};
+
+type CreateRequestBody = z.infer<typeof createDigitalProductsSchema>;
+
+export const POST = async (
+  req: AuthenticatedMedusaRequest<CreateRequestBody>,
+  res: MedusaResponse
+) => {
+  const query = req.scope.resolve(ContainerRegistrationKeys.QUERY);
+  const {
+    data: [shippingProfile],
+  } = await query.graph({
+    entity: "shipping_profile",
+    fields: ["id"],
+  });
+
+  const { result } = await createDigitalProductWorkflow(req.scope).run({
+    input: {
+      digital_product: {
+        name: req.validatedBody.name,
+        medias: req.validatedBody.medias.map((media) => ({
+          fileId: media.file_id,
+          mimeType: media.mime_type,
+          ...media,
+        })) as Omit<CreateDigitalProductMediaInput, "digital_product_id">[],
+      },
+      product: {
+        ...req.validatedBody.product,
+        shipping_profile_id: shippingProfile.id,
+      },
+    },
+  });
+
+  res.json({
+    digital_product: result.digital_product,
   });
 };
